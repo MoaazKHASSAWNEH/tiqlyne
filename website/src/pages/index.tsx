99a97c2be4187eb106f2383101bf4c3ba78923c6
MoaazKHASSAWNEH <72568802+MoaazKHASSAWNEH@users.noreply.github.com>
2026-07-09 12:09:35 +0200

    docs: use package version summary on homepage

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { packageVersionSummary } from '@site/src/data/packageVersions';
 import styles from './index.module.css';
 
 function HomepageHeader(): ReactNode {
@@ -14,7 +15,7 @@ function HomepageHeader(): ReactNode {
           src="/tiqlyne/img/logo.png"
           alt="Tiqlyne Motion Engine logo"
         />
-        <span className={styles.eyebrow}>Version 0.1.0</span>
+        <span className={styles.eyebrow}>{packageVersionSummary}</span>
         <Heading as="h1" className={styles.heroTitle}>
           Motion you can describe, inspect, and run anywhere.
         </Heading>
@@ -26,10 +27,7 @@ function HomepageHeader(): ReactNode {
           <Link className="button button--primary button--lg" to="/docs/getting-started">
             Get started
           </Link>
-          <Link
-            className="button button--secondary button--lg"
-            href="https://github.com/MoaazKHASSAWNEH/tiqlyne"
-          >
+          <Link className="button button--secondary button--lg" href="https://github.com/MoaazKHASSAWNEH/tiqlyne">
             View on GitHub
           </Link>
         </div>
